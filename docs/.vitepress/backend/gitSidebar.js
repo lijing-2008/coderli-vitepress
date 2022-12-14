ce60f02fc10d9f32de7aac6fd6fcbc0229b85467
lijing <jingli14@tsinghua.org.cn>
2022-12-14 22:53:35 +0800

    添加git的相关知识

--- docs/.vitepress/backend/gitSidebar.js
+++ docs/.vitepress/backend/gitSidebar.js
@@ -1,7 +1,7 @@
 import { walk } from "../scripts/utils";
 const baseDir = './docs/backend/git/'
 export const gitSidebar = [
-	walk(baseDir,'Git基本配置'),
-	walk(baseDir,'Git常用命令'),
-	walk(baseDir,'Git远程仓库'),
+	walk(baseDir,'git基本配置'),
+	walk(baseDir,'git常用命令'),
+	walk(baseDir,'git远程仓库'),
 ]
